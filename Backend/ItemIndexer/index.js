c118d291a384dc3298d2cd1de4ceeb859712b570
G_Moe <118219341+Mosal27@users.noreply.github.com>
2023-12-12 04:24:24 -0500

    Mini-Project 10 encription

--- Backend/ItemIndexer/index.js
+++ Backend/ItemIndexer/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import methodOverride from 'method-override';
 import { MongoClient } from 'mongodb';
 import mongoose from 'mongoose';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import bcrypt from 'bcryptjs';
+import session from 'express-session';
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
@@ -13,6 +18,7 @@ app.use(methodOverride('_method'));
 
 const uri = "mongodb://127.0.0.1:27017/";
 let db;
+
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -35,6 +41,46 @@ const itemSchema = new mongoose.Schema({
 });
 
 const Item = mongoose.model('Item', itemSchema);
+
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+const User = mongoose.model('User', userSchema);
+
+
+app.use(session({
+  secret: "black",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+      const user = await User.findById(id);
+      done(null, user);
+  } catch (err) {
+      done(err);
+  }
+});
+
+
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+      const user = await User.findOne({ username: username });
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+          return done(null, false, { message: 'Incorrect username or password.' });
+      }
+      return done(null, user);
+  }
+));
 
 app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`);
@@ -96,6 +142,31 @@ app.post('/api/items/update/:id', async (req, res) => {
     res.status(500).send('Error updating item in the database');
   }
 });
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+  try {
+      const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+      const newUser = new User({ username: req.body.username, password: hashedPassword });
+      await newUser.save();
+      res.redirect('/login');
+  } catch (error) {
+      console.error(error);
+      res.redirect('/register');
+  }
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
+  res.redirect('/');
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
